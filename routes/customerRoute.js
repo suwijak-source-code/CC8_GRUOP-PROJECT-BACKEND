e2782b0d4595224d9d1a253f6a9696f3c8b79c59
Thatsaphon <thatphon123@hotmail.com>
2021-05-21 08:00:16 +0700

    do some inventory order planting functions

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("../controllers/customerController");
+const customerController = require("../controllers/customerController");
 
-router.get("/", productController.getAllCustomers);
+router.get("/", customerController.getAllCustomers);
+router.get("/:id", customerController.getCustomerById);
+router.post("/", customerController.createCustomer);
+router.post("/address", customerController.createNewAddress);
+router.put("/", customerController.editCustomer);
 
 module.exports = router;
